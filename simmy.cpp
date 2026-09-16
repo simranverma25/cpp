d06f2d6b2098e08513d3ad1e74c62d9135ebe35f
simranverma25 <simmy7081@gmail.com>
2026-09-16 23:06:23 +0530

    second day of codding

--- simmy.cpp
+++ simmy.cpp
@@ -2,5 +2,31 @@
 using namespace std;
 
 int main() {
-    cout<<"My name is simran"<<endl;
+    // cout<<"My name is simran"<<endl;
+
+    int height ;
+    cout<<"Enter your height in feet"<<endl;
+    cin>>height;
+    int weight ;
+    
+    cout<<"Enter your weight in kg"<<endl;
+    cin>>weight;
+
+    if (height>5)
+    {
+        if (weight>70)
+        {
+            cout<<"you got a good BMI"<<endl;
+        }
+        else
+        {
+            cout<<"not good BMI"<<endl;
+        }
+        
+        
+    }
+    
+
+    
+    return 0;
 }
