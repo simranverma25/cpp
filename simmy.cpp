49413d17119052fd4ab9cd52a6e7560ad243b82c
simranverma25 <simmy7081@gmail.com>
2026-09-18 23:53:16 +0530

    fourth day of codding

--- simmy.cpp
+++ simmy.cpp
@@ -4,28 +4,53 @@ using namespace std;
 int main() {
     // cout<<"My name is simran"<<endl;
 
-    int height ;
-    cout<<"Enter your height in feet"<<endl;
-    cin>>height;
-    int weight ;
+    // int height ;
+    // cout<<"Enter your height in feet"<<endl;
+    // cin>>height;
+    // int weight ;
     
-    cout<<"Enter your weight in kg"<<endl;
-    cin>>weight;
+    // cout<<"Enter your weight in kg"<<endl;
+    // cin>>weight;
 
-    if (height>5)
-    {
-        if (weight>70)
-        {
-            cout<<"you got a good BMI"<<endl;
-        }
-        else
-        {
-            cout<<"not good BMI"<<endl;
-        }
+    // if (height>5)
+    // {
+    //     if (weight>70)
+    //     {
+    //         cout<<"you got a good BMI"<<endl;
+    //     }
+    //     else
+    //     {
+    //         cout<<"not good BMI"<<endl;
+    //     }
         
         
-    }
+    // }
     
+
+    // char day;
+    // cout<<"Enter a day"<<endl;
+    // cin>>day;
+     
+    // switch(day){
+    //     case '1' : cout<<"monday"<<endl;
+    //     break;
+    //     case '2' : cout<<"tuesday"<<endl;
+    //     break;
+    //     case '3' : cout<<"wednesday"<<endl;
+    //     break;
+    //     case '4' : cout<<"thursday"<<endl;
+    //     break;
+    //     case '5' : cout<<"friday"<<endl;
+    //     break;
+    //     case '6' : cout<<"saturday"<<endl;
+    //     break;
+    //     case '7' : cout<<"sunday"<<endl;
+    //     break;
+    // }
+
+     int month = 3;
+
+     (month>8)?cout<<"valid for entry":cout<<"not valid for entry";
 
     
     return 0;
